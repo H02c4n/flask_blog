aecdd3729c5cdcb4b0eccb99ae3a2253af396c7e
halil <svtr.hozcan@gmail.com>
2023-06-22 23:54:52 +0200

    login template

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegisterForm, LoginForm
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = '51206c77d50dd0e1363c493ba8483ca4'
 
 posts = [
     {
@@ -23,13 +27,30 @@ def home():
     return render_template('home.html', posts=posts)
 
 
-
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
 
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form  = RegisterForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@flask.com' and form.password.data == 'admin':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 
