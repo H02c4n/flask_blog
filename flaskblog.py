6bcfb5fe3aa083dea6af3a2f375e0aa75c2a80ca
halil <svtr.hozcan@gmail.com>
2023-06-22 13:31:11 +0200

    add bootstrap in the project

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 posts = [
@@ -26,7 +26,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title="About")
 
 
 
