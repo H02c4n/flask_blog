f1717f16210d66bcfb986554ed2223803b9ff780
halil <svtr.hozcan@gmail.com>
2023-06-22 11:28:59 +0200

    using templates folder

--- flaskblog.py
+++ flaskblog.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'Halil',
+        'title':'Blog post 1',
+        'content': 'First post content',
+        'date_posted':'June 21, 2023'
+    },
+    {
+        'author':'Cemal',
+        'title':'Blog post 2',
+        'content': 'Second post content',
+        'date_posted':'June 22, 2023'
+    }
+]
+
+
 @app.route("/")
-def hello():
-    return "<h1>Hello world</h1>"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
 
 
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 
 
